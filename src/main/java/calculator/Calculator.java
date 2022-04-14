ed33d2dd6cb72b683b5eed7b45906aa0a7367c46
karthikhegde05 <karthikhegde05@gmail.com>
2022-04-14 11:27:58 +0530

    updated playbook

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -8,7 +8,7 @@ import org.apache.logging.log4j.Logger;
 
 public class Calculator{
 
-    private static final Logger logger = LogManager.getLogger(Calculator.class);
+    private static final Logger logger= LogManager.getLogger(Calculator.class);
 
 
 
@@ -16,47 +16,45 @@ public class Calculator{
     }
 
     public double Sqrt(double num){
-        logger.info("[SQ ROOT] - " + num);
-        double result = Math.sqrt(num);
-        logger.info("[RESULT - SQ ROOT] - " + result);
-        return result;
+        logger.info("[square root] - " + num);
+        double resSqrt = Math.sqrt(num);
+        logger.info("[result of square root] - " + resSqrt);
+        return resSqrt;
     }
 
     public double factorial(double num){
-        logger.info("[FACTORIAL] - " + num);
+        logger.info("[factorial] - " + num);
         double pdt = 1;
         for(double l=1; l<=num; l++){
             pdt *= l;
         }
         
-        logger.info("[RESULT - FACTORIAL] - " + pdt);
+        logger.info("[result of factorial] - " + pdt);
         return pdt;
     }
 
     public double natLog(double num){
-        logger.info("[NATURAL LOG] - " + num);
-        double result = 0;
-        try {
-            if(num<0){
-                result = Double.NaN;
-                throw new ArithmeticException("Case of NaN 0.0/0.0");
-            }
-            else{
-                result = Math.log(num);
-            }
-        }catch(ArithmeticException error){
-            System.out.println("[EXCEPTION LOG] - Cannot find  log of negative numbers " + error.getLocalizedMessage());
-        }
-        logger.info("[RESULT - NATURAL LOG] - " + result);
+        logger.info("[natural log] - " + num);
+        double natlog = 0;
 
-        return result;
+        if(num<0){
+            natlog = Double.NaN;
+            System.out.println("natural logarithm is undefined for negative numbers");
+        }
+        else {
+            natlog = Math.log(num);
+        }
+
+        logger.info("[result of natural log] - " + natlog);
+
+        return natlog;
     }
 
-    public double power(double num, double exponent){
-        logger.info("[POWER] - " + num);
-        double result = Math.pow(num, exponent);
-        logger.info("[RESULT - POWER] - " + result);
-        return result;
+    public double power(double num, double exponentNum){
+        logger.info("[power] - " + num);
+        double resPower = Math.pow(num, exponentNum);
+        logger.info("[result of power] - " + resPower);
+        return resPower;
     }
 
 
@@ -65,9 +63,9 @@ public class Calculator{
         Scanner sc = new Scanner(System.in);
         
         double num1, num2;
-        do{
+        while(true){
             int choice;
-            System.out.println("Welcome to Calculator interface...\n Type your choice of operation\n" +  
+            System.out.println("Calculator program starting...\n Type your choice of operation\n" +  
             "Press 1 for finding square root\nPress 2 for finding factorial\nPress 3 for finding natural logarithm\n"
             + "Press 4 for finding power of a base raised to given exponent\n");
             
@@ -83,32 +81,28 @@ public class Calculator{
             
             switch(choice){
                 case 1:
-                    // square root
                     System.out.println("Enter a number: ");
                     num1 = sc.nextDouble();
-                    System.out.println("Square root of " + num1 + " = " + calculator.Sqrt(num1));
+                    System.out.println("square root of " + num1 + " = " + calculator.Sqrt(num1));
                     System.out.println("\n");
                     break;
 
                 case 2:
-                    // factorial
                     System.out.println("Enter a number: ");
                     num1 = sc.nextDouble();
-                    System.out.println("Factorial of " + num1 + " = " + calculator.factorial(num1));
+                    System.out.println("factorial of " + num1 + " = " + calculator.factorial(num1));
                     System.out.println("\n");
                     break;
 
                 case 3:
-                    // natural log
                     System.out.println("Enter a number: ");
                     num1 = sc.nextDouble();
-                    System.out.println("Natural logarithm of " + num1 + " = " + calculator.natLog(num1));
+                    System.out.println("natural logarithm of " + num1 + " = " + calculator.natLog(num1));
                     System.out.println("\n");
                     break;
 
                 case 4:
-                    // power
-                    System.out.println("Enter first number (base number): ");
+                    System.out.println("Enter first number base number: ");
                     num1 = sc.nextDouble();
                     System.out.println("Enter second number (power): ");
                     num2 = sc.nextDouble();
@@ -117,10 +111,10 @@ public class Calculator{
                     break;
                 
                 default:
-                    System.out.println("Exiting...\n");
+                    System.out.println("Program stopping...\n");
                     return;
             }
-        }while(true);
+        }
 
         return;
     }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -13,18 +13,21 @@ public class CalculatorTest {
     public void sqrtTruePositive(){
         assertEquals("Sqrt for true positive", 10, calculator.Sqrt(100), DELTA);
         assertEquals("Sqrt for true positive", 2, calculator.Sqrt(4), DELTA);
+        assertEquals("Sqrt for true positive", 4, calculator.Sqrt(16), DELTA);
     }
 
     @Test
     public void sqrtFalsePostitive(){
         assertNotEquals("Sqrt for false positive", 1, calculator.Sqrt(100), DELTA);
         assertNotEquals("Sqrt for false positive", 0,  calculator.Sqrt(81), DELTA);
+        assertNotEquals("Sqrt for false positive", 20,  calculator.Sqrt(16), DELTA);
     }
 
     @Test
     public void factTruePositive(){
         assertEquals("factorial for true positive", 24, calculator.factorial(4), DELTA);
         assertEquals("factorial for true positive", 120, calculator.factorial(5), DELTA);
+        assertEquals("factorial for true positive", 3628800, calculator.factorial(10), DELTA);
     }
     
 
@@ -32,6 +35,7 @@ public class CalculatorTest {
     public void factFalsePostitive(){
         assertNotEquals("factorial for false positive", 100, calculator.factorial(10), DELTA);
         assertNotEquals("factorial for false positive", 200,  calculator.factorial(4), DELTA);
+        assertNotEquals("factorial for false positive", 300,  calculator.factorial(6), DELTA);
     }
 
 
@@ -45,13 +49,16 @@ public class CalculatorTest {
     @Test
     public void logFalsePostitive(){
         assertNotEquals("natural log for false positive", 6, calculator.natLog(2.4), DELTA);
-        assertNotEquals("natural log for false positive", 7.3,  calculator.natLog(2.1), DELTA);
+        assertNotEquals("natural log for false positive", 7.3,  calculator.natLog(10), DELTA);
+        assertNotEquals("natural log for false positive", 7.3,  calculator.natLog(11), DELTA);
     }
 
     @Test
     public void powerTruePositive(){
         assertEquals("power for true positive", 16, calculator.power(2, 4), DELTA);
         assertEquals("power for true positive", 100, calculator.power(10, 2), DELTA);
+        assertEquals("power for true positive", 36, calculator.power(6, 2), DELTA);
+        assertEquals("power for true positive", 81, calculator.power(9, 2), DELTA);
     }
     
 
@@ -59,6 +66,7 @@ public class CalculatorTest {
     public void powerFalsePostitive(){
         assertNotEquals("power for false positive", 5, calculator.power(2, 4), DELTA);
         assertNotEquals("power for false positive", 1000,  calculator.power(10, 2), DELTA);
+        assertNotEquals("power for false positive", 100,  calculator.power(9, 2), DELTA);
     }
 
 
